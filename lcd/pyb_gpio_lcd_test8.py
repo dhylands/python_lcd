c3d7b3c2eeb0a4de7e729fe18cddbe541e5c70c4
Dave Hylands <dhylands@gmail.com>
2014-08-04 23:02:13 -0700

    Added test program for 8 bit mode.

---
+++ lcd/pyb_gpio_lcd_test8.py
@@ -0,0 +1,30 @@
+"""Implements a character based lcd connected via PCF8574 on i2c."""
+
+from pyb import Pin
+from pyb import delay, millis
+from pyb_gpio_lcd import GpioLcd
+
+def test_main():
+    """Test function for verifying basic functionality."""
+    print("Running test_main")
+    lcd = GpioLcd(rs_pin=Pin.board.Y12,
+                  enable_pin=Pin.board.Y11,
+                  d0_pin=Pin.board.Y1,
+                  d1_pin=Pin.board.Y2,
+                  d2_pin=Pin.board.Y3,
+                  d3_pin=Pin.board.Y4,
+                  d4_pin=Pin.board.Y5,
+                  d5_pin=Pin.board.Y6,
+                  d6_pin=Pin.board.Y7,
+                  d7_pin=Pin.board.Y8,
+                  num_lines=4, num_columns=20)
+    lcd.putstr("It Works!\nSecond Line\nThird Line\nFourth Line")
+    delay(3000)
+    lcd.clear()
+    count = 0
+    while True:
+        lcd.move_to(0, 0)
+        lcd.putstr("%7d" % (millis() // 1000))
+        delay(1000)
+        count += 1
+
