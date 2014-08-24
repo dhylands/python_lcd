4257e84f096a91806317ab6c1081b30c6aa620fe
Dave Hylands <dhylands@gmail.com>
2014-08-24 10:36:25 -0700

    Added comments for the wiring used.

--- lcd/pyb_gpio_lcd_test8.py
+++ lcd/pyb_gpio_lcd_test8.py
@@ -4,6 +4,36 @@ from pyb import Pin
 from pyb import delay, millis
 from pyb_gpio_lcd import GpioLcd
 
+# Wiring used for this example:
+#
+#  1 - Vss (aka Ground) - Connect to one of the ground pins on you pyboard.
+#  2 - VDD - I connected to VIN which is 5 volts when your pyboard is powerd vi USB
+#  3 - VE (Contrast voltage) - I'll discuss this below
+#  4 - RS (Register Select) connect to Y12 (as per call to GpioLcd)
+#  5 - RW (Read/Write) - connect to ground
+#  6 - EN (Enable) connect to Y11 (as per call to GpioLcd)
+#  7 - D0 - connect to Y1 (as per call to GpioLcd)
+#  8 - D1 - connect to Y2 (as per call to GpioLcd)
+#  9 - D2 - connect to Y3 (as per call to GpioLcd)
+# 10 - D3 - connect to Y4 (as per call to GpioLcd)
+# 11 - D4 - connect to Y5 (as per call to GpioLcd)
+# 12 - D5 - connect to Y6 (as per call to GpioLcd)
+# 13 - D6 - connect to Y7 (as per call to GpioLcd)
+# 14 - D7 - connect to Y8 (as per call to GpioLcd)
+# 15 - A (BackLight Anode) - Connect to VIN
+# 16 - K (Backlight Cathode) - Connect to Ground
+#
+# On 14-pin LCDs, there is no backlight, so pins 15 & 16 don't exist.
+#
+# The Contrast line (pin 3) typically connects to the center tap of a
+# 10K potentiometer, and the other 2 legs of the 10K potentiometer are
+# connected to pins 1 and 2 (Ground and VDD)
+#
+# The wiring diagram on the followig page shows a typical "base" wiring:
+# http://www.instructables.com/id/How-to-drive-a-character-LCD-displays-using-DIP-sw/step2/HD44780-pinout/
+# Add to that the EN, RS, and D0-D7 lines.
+
+
 def test_main():
     """Test function for verifying basic functionality."""
     print("Running test_main")
